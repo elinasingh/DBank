741cca9f3243db15162e8e8c7b7848730a040d75
Elina Singh <elinasingh1997@gmail.com>
2024-02-23 21:12:55 +0530

    Updating the current balance when its returns the result

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,1 +1,8 @@
+import { dbank } from "../../declarations/dbank";
 
+
+window.addEventListener("load", async function() {
+    // console.log("Finished Loading");
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerText = currentAmount;
+});
