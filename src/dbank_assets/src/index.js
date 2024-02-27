bfe700df1d385ecdac49c5562016c8285795837a
Elina Singh <elinasingh1997@gmail.com>
2024-02-27 20:28:17 +0530

    Finalising transaction

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -3,19 +3,38 @@ import { dbank } from "../../declarations/dbank";
 
 window.addEventListener("load", async function() {
      //  console.log("Finished Loading");
-     const currentAmount = await dbank.checkBalance();
-     document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+     update();
 });
 
 document.querySelector("form").addEventListener("submit", async function(event) {
      event.preventDefault();
      // console.log("submitted");
 
+     const button = event.target.querySelector("#submit-btn");
+
      const inputAmount = parseFloat(document.getElementById("input-amount").value);
      const outputAmount = parseFloat(document.getElementById("wthdrawal-amount").value);
 
-     await dbank.topUp(inputAmount);
+     button.setAttribute("disabled", true);
 
+     if (document.getElementById("input-amount").value.length != 0) {
+          await dbank.topUp(inputAmount);
+     }
+
+     if (document.getElementById("wthdrawal-amount").value.length != 0) {
+          await dbank.withdraw(outputAmount);
+     }
+
+     await dbank.compound();
+
+     update();
+
+     document.getElementById("input-amount").value = "";
+     document.getElementById("wthdrawal-amount").value = "";
+     button.removeAttribute("disabled");
+});
+
+async function update() {
      const currentAmount = await dbank.checkBalance();
      document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
-});
+};
